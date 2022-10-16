6c1e637f12693c4b59084448895129233f7c9a17
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-10-16 22:29:43 +0900

    capture turtle move and need to figure out statements moves

--- turtle/app.R
+++ turtle/app.R
@@ -31,7 +31,8 @@ ui <- fluidPage(
         mainPanel(
 
             # Output: Turtle Playground ----
-            plotOutput(outputId = "turtlePlot")
+            plotOutput(outputId = "turtlePlot"),
+            verbatimTextOutput(outputId = "turtle_program"),
 
         )
     )
@@ -40,19 +41,39 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-    turtle <- reactiveValues(position = 0)
+    turtle <- reactiveValues(position = 0,
+                             program = "")
 
     observeEvent(input$move, {
         turtle$position <- turtle$position + input$move
     })
 
+    ### 주사위 던지기 이력 ------------------
+    program_val <- c()
+
+    program_history <- reactive({
+        program_val <<- c(program_val,glue::glue("turtle_move({turtle$position})"))
+    })
+
+
+    observeEvent(input$move, {
+        turtle$program <- glue::glue("turtle_move({turtle$position})")
+    })
 
     output$turtlePlot <- renderPlot({
 
         turtle_init(mode = "clip")
 
-        turtle_move(dist = turtle$position)
+        turtle_go <- rlang::parse_expr(program_history())
+        eval(turtle_go)
         turtle_right(angle = input$angle)
+
+    })
+
+    output$turtle_program <- renderText({
+
+        # print(glue::glue("{turtle$program}"))
+        print(program_history())
 
     })
 
